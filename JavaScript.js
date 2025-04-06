dac3404879fd4b64116391d5a03eeb27a2b6d65b
Ayla <danniebeltran14@gmail.com>
2025-04-06 09:41:29 -0500

    Pagina Principal semifinal
    Se realizan avances de la estructura

--- JavaScript.js
+++ JavaScript.js
@@ -6,3 +6,10 @@ function moveSlide(direction) {
     currentSlide = (currentSlide + direction + slides.length) % slides.length; // Calcula el índice de la nueva diapositiva
     slides[currentSlide].classList.add("active"); // Muestra la nueva diapositiva
 }
+// Ejemplo de comportamiento al hacer clic
+document.querySelectorAll('.icon').forEach(icon => {
+    icon.addEventListener('click', () => {
+      alert('¡Red social próximamente disponible!');
+    });
+  });
+  
